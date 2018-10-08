c821b686276651a62a54bba6c2e3a4cc823bfaa6
Benda Klara <perfectyello@gmail.com>
2018-10-08 10:40:20 +0200

    first commit

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+	"facebook_app" 		: 			"Celeb neked",
+	"facebook_api_key" 		: 			"515440732215602",
+	"facebook_api_secret"	:				"1a23c4fbf1ba135ca93fc84992cec1ca",
+	"callback_url"				:				"https://datadatbot.tk:5006/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"							:			"localhost",
+	"username"						:				"root",
+	"password"						:				"CeMwQNXH75arf4K7",
+	"database"						:				"celeb"
+}
